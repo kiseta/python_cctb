5a08428f2073711c78e62b89472fa3db5c0350ef
kiseta <kiseta@gmail.com>
2022-01-21 21:15:18 -0800

    lab11 updated tuple_set_dictionry.py renamed to list_tuple_set_dictionary.py

--- practice/lab11_tatyana.py
+++ practice/lab11_tatyana.py
@@ -5,34 +5,36 @@ __author__ = 'tk'
 # 2. Simulate error and catch it using except.
 # 3. Use finally keyword to finalize the statement.
 
-# EXAMPEL 1 List IndexError
+# EXAMPLE 1 List IndexError
 try:
-    twilight_saga = ['Twilgiht', 'New Moon', 'Eclipse', 'Breaking Dawn']  # make a list with square brackets
-    print('--------- correct ------------')
+    twilight_saga = ['Twilight', 'New Moon', 'Eclipse', 'Breaking Dawn']  # make a list with square brackets
+    print('--------- #1 correct ------------')
     print(twilight_saga[3])
-    print('--------- error ------------')
-    print(twilight_saga[4])  # print 4rd item from the list - generate error
+    print('--------- #1 error ------------')
+    print(twilight_saga[4])  # print 4TH item from the list - generate error
 except IndexError as ie:
     print(f'IndexError: {ie}, check your item index')
 finally:
     print('---------------- Program is done----------------')
 
-# EXAMPEL 2 Dictionary - print the first item error
+# EXAMPLE 2 Dictionary - print the first item error
 try:
     twilight_character = {
         'vampire': 'Edward',
         'human': 'Bella',
-        'werewolfe': 'Jacob',
+        'werewolf': 'Jacob',
         'hybrid': 'Renesmee'
     }  # make a dictionary with curly braces
 
-    print('--------- error ------------')
-    print(twilight_character.keys()[0]) # print the value from first type - generate error
-    print('--------- error ------------')
-    print(twilight_character.keys(0)) # print the value from first type - generate error
-    print('--------- correct ------------')
+    # print('--------- error ------------')
+    # print(twilight_character.keys()[0]) # print the value from first type - generate error
+    # print('--------- error ------------')
+    # print(twilight_character.keys(0)) # print the value from first type - generate error
+    print('--------- #2 correct ------------')
     print(twilight_character.get('vampire'))  # print the value from first type
-    print('--------- error ------------')
+    print(twilight_character.keys())
+    print(twilight_character.values())
+    print('--------- #2 error ------------')
     print(twilight_character.keys('vampire'))  # print the value from first type - generate error
 except TypeError as te:
     print(f'TypeError: {te}, check your item type')
